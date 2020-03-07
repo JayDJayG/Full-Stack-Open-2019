9de5323172041d21e89641f3e9dcf9ea07d500a5
j.d.guzman.v@gmail.com <jairoguzman@Jairos-MacBook-Air.local>
2020-03-06 18:08:50 -0600

    Part 1.2 completed coded refactorized

--- part1/part1.1/src/index.js
+++ part1/part1.1/src/index.js
@@ -9,10 +9,24 @@ return(
     </div>
 )
 }
+
+const Part = (props) =>
+{
+    return(
+        <div>
+            <p>{props.part} {props.exercise}</p>
+        </div>    
+    )
+    
+}
 const Content = (props) => {
     return(
         <div>
-        {props.part} {props.exercise}
+
+            <Part part = {props.part1} exercise = {props.exercise1}/> 
+            <Part part = {props.part2} exercise = {props.exercise2}/> 
+            <Part part = {props.part3} exercise = {props.exercise3}/> 
+
         </div>   
     )
 }
@@ -38,7 +52,10 @@ const App = () => {
     return (
       <div>
         <Header course={'course'} />
-        <Content part={'part 1'} exercise={12}/>
+        <Content part1={'part 1'} exercise1={exercise1}
+                part2={'part 2'} exercise2={exercise2}
+                part3={'part 3'} exercise3={exercise3}
+        />
         <Total total= {exercise1 + exercise2 + exercise3}/>
       </div>
     )
